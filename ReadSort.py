721a9686bdbb057c69139f9372c961867dd43b04
rodrigoduartebraga1@gmail.com <rodrigoduartebraga1@gmail.com>
2024-04-15 21:29:24 +0100

    Fixed a bug with some good quality data being misidentified when using 'classifier' or 'chan_gen', and added a new model_type called breakdown, that returns a dictionary with detailed information about subject, situation, and channel name alongside the EEG data

--- ReadSort.py
+++ ReadSort.py
@@ -3,6 +3,9 @@ import os
 import pandas as pd
 import numpy as np
 
+# for key stuff
+import re
+
 def structure_data(model_type): #change this to take arguments of type (classifier, location generator, data generator, etc)
     # Directory containing your EEG files
     eeg_directory = r"E:\Code snippets\IntraOp Data"
@@ -182,6 +185,103 @@ def structure_data(model_type): #change this to take arguments of type (classifi
         for i, (key, value) in enumerate(binary_channel_dict.items()):
             new_dict[i] = value # Add 1 to the index to start from 1
         return new_dict#binary_channel_dict
+    
+    elif model_type == "hitorie":
+        eeg_directory = r"E:\Code snippets\IntraOp Data"
+        # Initialize an empty dictionary to store EEG data
+        eeg_data_dict_injured = {}
+        eeg_data_dict_healthy = {}
+        hitorie = {"healthy": {}, "injured": {}}
+        # Iterate through all files in the directory and its subdirectories
+        for foldername, subfolders, filenames in os.walk(eeg_directory):
+            for filename in filenames:
+                # Check if the file is an EEG file (you may want to add more conditions)
+                if filename.endswith('_ieeg.vhdr'):
+                    # Extract patient and situation information from the file path
+                    path_parts = foldername.split(os.sep)
+                    patient_id = None
+                    situation_id = None
+                    
+                    vhdr_file = os.path.join(foldername, filename)
+                    filename_channel_quality = vhdr_file.replace('acute_ieeg.vhdr', 'acute_channels.tsv')
+                    filename_channel_resect = vhdr_file.replace('task-acute_ieeg.vhdr', 'electrodes.tsv')
+                    for part in path_parts:
+                        if part.startswith('sub-'):
+                            patient_id = part
+                        elif part.startswith('ses-'):
+                            situation_id = part
+        
+                    # Check if patient and situation information is found
+                    if patient_id and situation_id:
+                        # Create the dictionary key
+                        key = f'{patient_id}_{situation_id}'
+                        
+                        
+                        file_path = os.path.join(foldername, filename)
+                        
+                        # Add the EEG information to the dictionary
+                        #eeg_data_dict[key] = {'file_path': file_path}                
+                        
+                        # Read the channels.tsv file
+                        channels_info = pd.read_csv(filename_channel_quality, delimiter='\t')
+                        # Extract good channels based on the "sampling_frequency" column
+                        good_channels = channels_info.loc[channels_info['status_description'] == 'included', 'name'].tolist()
+                        
+                        # Read the channels.tsv file
+                        channels_info = pd.read_csv(filename_channel_resect, delimiter='\t')
+                        # Extract good channels based on the "sampling_frequency" column
+                        injured_channels = channels_info.loc[(channels_info['resected'] == 'yes') & (channels_info['edge'] == 'no'), 'name'].tolist()
+                        
+                        common_channels_injured = list(set(good_channels).intersection(injured_channels))
+                        if common_channels_injured != []:
+                            raw = mne.io.read_raw_brainvision(vhdr_file, preload=True)
+                            raw_injured = raw.pick_channels(ch_names=common_channels_injured)#good_channels) #apparently pick_channels is a legacy function and should use inst.pick or something
+                            # Store the raw data in the dictionary with the constructed key
+                            eeg_data_dict_injured[key] = raw_injured
+                        
+                        # Read the channels.tsv file
+                        channels_info = pd.read_csv(filename_channel_resect, delimiter='\t')
+                        # Extract good channels based on the "sampling_frequency" column
+                        healthy_channels = channels_info.loc[(channels_info['resected'] == 'no') & (channels_info['edge'] == 'no'), 'name'].tolist()
+                        
+                        common_channels_healthy = list(set(good_channels).intersection(healthy_channels))
+                        if common_channels_healthy != []:
+                            raw = mne.io.read_raw_brainvision(vhdr_file, preload=True)
+                            raw_healthy = raw.pick_channels(ch_names=common_channels_healthy)
+                            eeg_data_dict_healthy[key] = raw_healthy
+ 
+                        # Example function to extract channels from raw data
+                        def extract_channels(raw_object):
+                            channels_dict = {}
+                            ch_names = raw_object.ch_names  # Get the channel names
+
+                            for channel_idx, ch_name in enumerate(ch_names):
+                                channel_data = raw_object.get_data(picks=channel_idx)
+                                channels_dict[ch_name] = channel_data
+
+                            return channels_dict
+                        
+                        if patient_id not in hitorie and common_channels_healthy != []:
+                            # If the patient ID doesn't exist, create a new entry
+                            channels = extract_channels(raw_healthy)
+                            hitorie["healthy"][patient_id] = {situation_id: channels}
+                        elif common_channels_healthy != []:
+                            # If the patient ID already exists, add the new situation code and data
+                            channels = extract_channels(raw_healthy)
+                            hitorie["healthy"][patient_id][situation_id] = channels
+                            
+                        if patient_id not in hitorie and common_channels_injured != []:
+                            # If the patient ID doesn't exist, create a new entry
+                            channels = extract_channels(raw_injured)
+                            hitorie["injured"][patient_id] = {situation_id: channels}
+                        elif common_channels_injured != []:
+                            # If the patient ID already exists, add the new situation code and data
+                            channels = extract_channels(raw_injured)
+                            hitorie["injured"][patient_id][situation_id] = channels
+         
+
+        
+        return hitorie
 
 
 
